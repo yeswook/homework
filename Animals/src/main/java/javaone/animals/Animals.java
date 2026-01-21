7e2ad3e22a1d8736da0e701ad178de022d92b261
bsquocthien <bsquocthien@gmail.com>
2026-01-21 08:31:24 +0700

    Add interactive menu to KETHUA main

--- Animals/src/main/java/javaone/animals/Animals.java
+++ Animals/src/main/java/javaone/animals/Animals.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package javaone.animals;
 
 import Models.*;
@@ -12,12 +11,22 @@ import Models.*;
  */
 public class Animals {
 
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Animal");
+        animal.Keu();
+        Animal_2chi haichi = new Animal_2chi("2 chi");
+        //haichi.Keu();
+        Chicken chicken = new Chicken("Chicken");
+        chicken.Keu();
+        Duck duck = new Duck("Duck");
+        duck.Keu();
+
         Animal ani1 = new Animal("Ani1");
         Animal ani2 = new Chicken("Ani2");
         Animal ani3 = new Duck("Ani3");
         ani1.Keu();
         ani2.Keu();
-        ani3.Keu();     
+        ani3.Keu();
     }
 }
